671f6018bf99134237f4eec0afaf70ec9189b257
harsh <hanzo4679@gmail.com>
2025-06-21 04:00:28 +0530

    mod: formatting

--- template.cpp
+++ template.cpp
@@ -1,24 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solve() {
 
-}
+void solve() {}
 
 int main() {
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
   ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+  cin.tie(nullptr); cout.tie(nullptr);
 
-  int nt;
-  cin >> nt;
-  while (nt--) {
-    solve();
-  }
+  int tests;
+  cin >> tests;
+  while (tests--) solve();
 
   return 0;
 }
-
