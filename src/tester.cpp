659e3301825c6d89cca0a8d7aa61400e74af9a3c
harsh <hanzo4679@gmail.com>
2025-06-21 02:38:06 +0530

    modification: depracated build.cpp

--- src/tester.cpp
+++ src/tester.cpp
@@ -5,6 +5,7 @@
 #include <sys/wait.h>   // for waitpid
 #include <unistd.h>     // for types
 
+#include <cstdio>    // for perror
 #include <cstdlib>   // for exit()
 #include <fstream>   // for fstream
 #include <iostream>  // for std::cerr
@@ -52,6 +53,46 @@ Tester::Tester(int argc, char* argv[], std::string& cwd)
         exit(1);
     }
   }
+}
+
+extern char** environ;
+std::optional<buildErr> Tester::build() {
+  std::string sourcefile = m_filename + ".cpp";
+  char* args[] = {(char*)"g++", (char*)sourcefile.c_str(), (char*)"-o",
+                  (char*)m_filename.c_str(), NULL};
+  pid_t build_id;
+  if (posix_spawnp(&build_id, "g++", NULL, NULL, args, environ) != 0) {
+    perror("build: posix failed");
+    return buildErr::PROCESSING_ERR;
+  }
+  int status;
+  if (waitpid(build_id, &status, 0) < 0) {
+    perror("build: wait failed");
+    return buildErr::PROCESSING_ERR;
+  }
+
+  if (WIFEXITED(status))
+    std::cerr << BRIGHT_YELLOW_FG
+              << "build exit status: " << WEXITSTATUS(status) << COLOR_END
+              << '\n';
+  else {
+    std::cerr << "build: unknown error occured\n" << '\n';
+    return buildErr::PROCESSING_ERR;
+  }
+
+  if (m_ship) {
+    std::fstream readsrc{sourcefile, std::ios::in};
+    std::fstream cp{m_ship.value() + "ship.cpp", std::ios::out};
+
+    if (!readsrc || !cp)
+      std::cerr << "Unable to ship!\n";
+    else {
+      cp << readsrc.rdbuf();
+      std::cerr << GREEN_FG << "\t shipped!\n";
+    }
+  }
+
+  return {};
 }
 
 std::optional<status> Tester::runTests(std::ofstream& report) {
